57cf27131ed5fcd653634aebffe391439d7b2b2c
davikrig-ha <daviluia2@gmail.com>
2022-05-03 17:17:24 -0300

    add

--- cronometro/src/App.js
+++ cronometro/src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 import "./App.css";
 
-
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      numero: 0
+    }
+
+    this.iniciar = this.iniciar.bind(this);
+    this.parar = this.parar.bind(this);
+  }
+
+  iniciar() {
+   
+  }
+
+  parar() {
+
+  }
+
   render() {
     return (
       <div className="container">
-        <img className="img"  src={require("./assets/cronometro.png")} alt="image" />
-        <a className="timer">0.0</a>
+        {/* <img className="img"  src={require("./assets/cronometro.png").default} alt="" /> */}
+        <a href="/#" className="timer">{this.state.numero.toFixed(1)}</a>
         <div className="area">
-          <a>Iniciar</a>
-          <a>Parar</a>
+          <a onClick={this.iniciar}  className="btn" href="/#">Iniciar</a>
+          <a onClick={this.parar}  className="btn" href="/#">Parar</a>
         </div>
       </div>
     );
